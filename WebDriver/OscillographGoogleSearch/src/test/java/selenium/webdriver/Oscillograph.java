14b364b6edf2fd9f1933c50241be37c729b6c70e
Nadiia Poizdnyk <nadiia.poizdnyk@gmail.com>
2016-02-04 16:54:59 +0200

    OscillographGoogleSearch is updated. Comments are added.

--- WebDriver/OscillographGoogleSearch/src/test/java/selenium/webdriver/Oscillograph.java
+++ WebDriver/OscillographGoogleSearch/src/test/java/selenium/webdriver/Oscillograph.java
@@ -10,10 +10,12 @@ package selenium.webdriver;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import java.io.File;
 import java.io.IOException;
-import java.lang.String;
 import java.util.concurrent.TimeUnit;
 
 
@@ -22,47 +24,54 @@ public class Oscillograph {
     private WebDriver driver;
     boolean elementPresent = false;
     private int pageIndex = 1;
-    private String url;
 
     @BeforeTest
+    // open the FF browser and go to Google search engine (https://www.google.com.ua/)
     public void setUp()throws Exception{
 
         driver = new FirefoxDriver();
-        driver.get(url="https://www.google.com.ua/");
-        //driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.google.com.ua/");
+        //driver.manage().window().maximize();          //maximize the browser window
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);    //waiter for 30 seconds
     }
 
     @Test
     public void googleSearch()throws Exception {
 
+        // clear the Search edit box (by id="lst-ib") and enter "осциллограф"
         driver.findElement(By.id("lst-ib")).clear();
         WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("осциллограф");
-        driver.findElement(By.xpath(".//*[@id='sblsbb']/button")).click();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath(".//*[@id='sblsbb']/button")).click();     // click on Go button
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);     //waiter for 5 seconds
 
+
+        // find element which contains text "vit.ua"
         while (elementPresent != true) {
             elementPresent = isElementPresent(By.xpath(".//div[@class=\"f kv _SWb\"]/cite[contains(text(),\"vit.ua\")]"));
 
-
+            // if element is provided on the search engine page, then print out the page number
             if (elementPresent == true) {
 
-                System.out.println("VIT.ua is on page:" + pageIndex);
+                System.out.println("VIT.ua is provided on page:" + pageIndex);
                 screenShot();
-            } else if (elementPresent == false) {
+            } else if (elementPresent == false) {       // if element is not provided, then click on Next button
                 pageIndex++;
-                System.out.println("Navigating on page " + pageIndex);
+                System.out.println("Navigating on page " + pageIndex);  // print out the page# where the navigating was done
+
+
+                // exception
                 try {
-                    driver.findElement(By.xpath(".//*[@id='pnnext']/span[1]")).click();
+                    driver.findElement(By.xpath(".//*[@id='pnnext']/span[1]")).click();     //click on Next button
                 } catch (NoSuchElementException e) {
-                    System.out.println("Sorry. VIT.ua not found!");
+                    System.out.println("Sorry. VIT.ua not found!");     //warning window, when element is not provided at all
                     return;
                 }
             }
         }
     }
-
+        // the method which is called while searching of element which contains text "vit.ua"
+        // elementPresent verification
         private boolean isElementPresent(By by){
             try{
                 driver.findElement(by);
@@ -71,6 +80,8 @@ public class Oscillograph {
                 return false;
             }
         }
+
+        // the method which is called for screenshot taking
         public void screenShot() {
             File screenshot = (File) ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
             try {
@@ -80,7 +91,8 @@ public class Oscillograph {
                 e.printStackTrace();
             }
         }
-    @AfterClass
+
+    @AfterClass     // close the browser
     public void tearDown() throws Exception {
         driver.quit();
     }
