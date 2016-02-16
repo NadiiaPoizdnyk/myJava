92f72fadb389a9a3e2faa53f432805993d31b46a
Nadiia Poizdnyk <nadiia.poizdnyk@gmail.com>
2016-02-16 18:00:06 +0200

    Updates: Pattern & Matcher

--- Java/JavaTasks/src/decisionMakingStatements/switchStatement/ExamAssessment.java
+++ Java/JavaTasks/src/decisionMakingStatements/switchStatement/ExamAssessment.java
@@ -1,6 +1,8 @@
 package decisionMakingStatements.switchStatement;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /** A student passed the exam. Enter student's assessment on 10 points system.
  *  The program will give its own assessment of the student's work.
@@ -9,8 +11,21 @@ public class ExamAssessment {
     public static void main(String[]args){
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Type a number: ");
-        int a = sc.nextInt();
+
+        int a = 0;
+        String str = null;
+        Pattern pattern = Pattern.compile("[1-9]{1}|[01]{2,2}");
+        Matcher matcher = null;
+
+        do {
+            System.out.println("Type a number: ");
+            str = sc.nextLine();
+            matcher = pattern.matcher(str);
+            if (!matcher.matches()) System.out.println("Sorry! Invalid value has been entered");
+
+        } while (!matcher.matches());
+
+        a = Integer.parseInt(str);
         sc.close();
 
         switch (a){
@@ -34,8 +49,10 @@ public class ExamAssessment {
             case 10:
                 System.out.println("Grade: " + a + "-excellent");
                 break;
+            /*
             default:
                 System.out.println("Wrong grade");
+                */
         }
     }
 }
--- Java/JavaTasks/src/decisionMakingStatements/switchStatement/NumbersAndWords.java
+++ Java/JavaTasks/src/decisionMakingStatements/switchStatement/NumbersAndWords.java
@@ -1,6 +1,8 @@
 package decisionMakingStatements.switchStatement;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /** The user types a number from 1 to 5.
  *  The program converts numbers into words, or reports that the user has entered an invalid number.
@@ -9,29 +11,43 @@ public class NumbersAndWords {
     public static void main(String[]args){
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Type a number");
-        int a = sc.nextInt();
+
+        int a = 0;
+        String string = null;
+        Pattern pattern = Pattern.compile("[1-5]{1}");
+        Matcher matcher = null;
+
+        do {
+            System.out.print("Type a number: ");
+            string = sc.nextLine();
+            matcher = pattern.matcher(string);
+            if (!matcher.matches()) System.out.println("Sorry! Invalid value has been entered");
+        }while (!matcher.matches());
+
         sc.close();
+        a = Integer.parseInt(string);
 
         switch (a){
             case 1:
-                System.out.println("One");
+                System.out.println("\nOne");
                 break;
             case 2:
-                System.out.println("Two");
+                System.out.println("\nTwo");
                 break;
             case 3:
-                System.out.println("Three");
+                System.out.println("\nThree");
                 break;
             case 4:
-                System.out.println("Four");
+                System.out.println("\nFour");
                 break;
             case 5:
-                System.out.println("Five");
+                System.out.println("\nFive");
                 break;
+            /*
             default:
-                System.out.println("Wrong number");
+                System.out.println("\nWrong number");
                 break;
+                */
         }
     }
 }
